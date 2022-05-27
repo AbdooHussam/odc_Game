e3986ca0b5036ea9b1e3ce55d850a5fa9234e731
AbdooHussam <Booodyhussamh@gmail.com>
2022-05-27 13:45:33 +0200

    first commit

---
+++ lib/flipcardgame.dart
@@ -0,0 +1,269 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flipcard/homepage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'data.dart';
+import 'dart:async';
+
+class FlipCardGane extends StatefulWidget {
+  final Level _level;
+
+  FlipCardGane(this._level);
+
+  @override
+  _FlipCardGaneState createState() => _FlipCardGaneState(_level);
+}
+
+class _FlipCardGaneState extends State<FlipCardGane> {
+  _FlipCardGaneState(this._level);
+
+  int _previousIndex = -1;
+  bool _flip = false;
+  bool _start = false;
+
+  bool _wait = false;
+  Level _level;
+  Timer _timer;
+  int _time = 5;
+  int _left;
+  bool _isFinished;
+  List<String> _data;
+
+  List<bool> _cardFlips;
+  List<GlobalKey<FlipCardState>> _cardStateKeys;
+
+  Widget getItem(int index) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey[100],
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black45,
+              blurRadius: 3,
+              spreadRadius: 0.8,
+              offset: Offset(2.0, 1),
+            )
+          ],
+          borderRadius: BorderRadius.circular(5)),
+      margin: const EdgeInsets.all(4.0),
+      child: Image.asset(_data[index]),
+    );
+  }
+
+  startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (t) {
+      setState(() {
+        _time = _time - 1;
+      });
+    });
+  }
+
+  void restart() {
+    startTimer();
+    _data = getSourceArray(
+      _level,
+    );
+    _cardFlips = getInitialItemState(_level);
+    _cardStateKeys = getCardStateKeys(_level);
+    _time = 5;
+    _left = (_data.length ~/ 2);
+
+    _isFinished = false;
+    Future.delayed(const Duration(seconds: 6), () {
+      setState(() {
+        _start = true;
+        _timer.cancel();
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    restart();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isFinished
+        ? Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              leading: IconButton(
+                  iconSize: 35,
+                  color: Colors.lightGreenAccent,
+                  onPressed: () {
+                    _time = 0;
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => HomePage()),
+                        ModalRoute.withName('/'));
+                  },
+                  icon: const Icon(Icons.arrow_back)),
+            ),
+            body: Center(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    restart();
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: 200,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: const Text(
+                    "Replay",
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              leading: IconButton(
+                  iconSize: 35,
+                  color: Colors.lightGreenAccent,
+                  onPressed: () {
+                    _isFinished = true;
+                    _time = 0;
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => HomePage()),
+                        ModalRoute.withName('/'));
+                  },
+                  icon: const Icon(Icons.arrow_back)),
+            ),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: _time > 0
+                          ? Text(
+                              '$_time',
+                              style: Theme.of(context).textTheme.headline3,
+                            )
+                          : Text(
+                              'Left:$_left',
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                        ),
+                        itemBuilder: (context, index) => _start
+                            ? FlipCard(
+                                key: _cardStateKeys[index],
+                                onFlip: () {
+                                  if (!_flip) {
+                                    _flip = true;
+                                    _previousIndex = index;
+                                  } else {
+                                    _flip = false;
+                                    if (_previousIndex != index) {
+                                      if (_data[_previousIndex] !=
+                                          _data[index]) {
+                                        _wait = true;
+
+                                        Future.delayed(
+                                            const Duration(milliseconds: 1500),
+                                            () {
+                                          _cardStateKeys[_previousIndex]
+                                              .currentState
+                                              .toggleCard();
+                                          _previousIndex = index;
+                                          _cardStateKeys[_previousIndex]
+                                              .currentState
+                                              .toggleCard();
+
+                                          Future.delayed(
+                                              const Duration(milliseconds: 160),
+                                              () {
+                                            setState(() {
+                                              _wait = false;
+                                            });
+                                          });
+                                        });
+                                      } else {
+                                        _cardFlips[_previousIndex] = false;
+                                        _cardFlips[index] = false;
+                                        print(_cardFlips);
+
+                                        setState(() {
+                                          _left -= 1;
+                                        });
+                                        if (_cardFlips
+                                            .every((t) => t == false)) {
+                                          print("Won");
+                                          Future.delayed(
+                                              const Duration(milliseconds: 160),
+                                              () {
+                                            setState(() {
+                                              _isFinished = true;
+                                              _start = false;
+                                            });
+                                          });
+                                        }
+                                      }
+                                    }
+                                  }
+                                  setState(() {});
+                                },
+                                flipOnTouch: _wait ? false : _cardFlips[index],
+                                direction: FlipDirection.HORIZONTAL,
+                                front: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.circular(5),
+                                      boxShadow: const [
+                                        BoxShadow(
+                                          color: Colors.black45,
+                                          blurRadius: 3,
+                                          spreadRadius: 0.8,
+                                          offset: Offset(2.0, 1),
+                                        )
+                                      ]),
+                                  margin: const EdgeInsets.all(4.0),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Image.asset(
+                                      "assets/animalspics/quest.png",
+                                    ),
+                                  ),
+                                ),
+                                back: getItem(index))
+                            : getItem(index),
+                        itemCount: _data.length,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flipcard/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Abdelrahman Hossam',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
